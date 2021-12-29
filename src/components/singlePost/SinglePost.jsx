3c292703afa476f55c75f96c93ab8d3862eb801b
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-29 15:31:38 -0600

    style singlepost"

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -16,6 +16,15 @@ export default function SinglePost() {
                         <i className="singlePostIcon far fa-trash-alt"></i>
                     </div>
                 </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Jeff</b></span>
+                    <span className="singlePostDate">Date <b>1 hour ago</b></span>
+                </div>
+                <p className="singlePostDesc">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus corrupti nihil nostrum voluptatem, sed tenetur vitae molestias deleniti nulla, esse eveniet soluta debitis beatae aspernatur placeat adipisci, ipsa natus laborum!
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque magnam cupiditate eligendi ex a perspiciatis id, earum voluptatibus asperiores at quia temporibus reprehenderit tempora hic ab. Reiciendis quasi enim placeat.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga enim, obcaecati dignissimos architecto ad itaque incidunt consequuntur ex autem minus sequi aperiam placeat, quia quaerat corporis tempora, eaque dolores veniam? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, molestiae at nisi nam delectus quae debitis modi dolor voluptatem, soluta voluptate maxime? Est dolorum nam, error laudantium odit commodi eos.
+                </p>
             </div>
         </div>
     )
