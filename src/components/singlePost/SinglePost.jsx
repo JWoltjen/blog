9f503eb22df2aa50e026cf6c145896865e71926f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-29 15:02:47 -0600

    create singlepost component layout

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,20 @@ import "./SinglePost.css"
 export default function SinglePost() {
     return (
         <div className="singlePost">
-            SinglePost
+            <div className="singlePostWrapper">
+                <img 
+                    src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+                    alt="" 
+                    className="singlePostImg"
+                />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit amet.
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                </h1>
+            </div>
         </div>
     )
 }
