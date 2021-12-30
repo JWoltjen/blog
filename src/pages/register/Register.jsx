19c66e50c29e475c078f322db015067f57db1caf
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 16:41:11 -0600

    create register page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,23 @@
+import './Register.css'
+
+export default function Register() {
+    return (
+        <div className="login">
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label> Email</label>
+                <input 
+                    className="loginInput"
+                    type="text" 
+                    placeholder="enter your email"/>
+                <label> Password</label>
+                <input 
+                    type="password" 
+                    className="loginInput"
+                    placeholder="enter your password..."/>
+                <button className="loginButton">Login</button>
+            </form>
+            <button className="loginRegisterButton">Register</button>
+        </div>
+    )
+}
