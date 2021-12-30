aab6e433c9abb47cb8349282bd5a9a7af9b94cd2
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 16:06:01 -0600

    debug profile picture not bringing up file selection on click

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -16,10 +16,15 @@ export default function Settings() {
                             src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                             alt=""
                         />
-                        <label className="fileInput">
-                            <i className='settingsPPIcon far fa-user-circle'></i>
+                        <label htmlFor="fileInput">
+                            <i className='settingsPPIcon far fa-user-circle'></i>{" "}
                         </label>
-                        <input type="file" id="fileInput" style={{display: 'none'}}/>
+                        <input 
+                            type="file" 
+                            id="fileInput" 
+                            style={{display:'none'}}
+                            className="settingsPPInput"
+                        />
                     </div>
                     <label>Username</label>
                     <input type="text" placeholder="Jeff"/>
@@ -27,7 +32,7 @@ export default function Settings() {
                     <input type="text" placeholder="Jeff.Woltjen@gmail.com"/>
                     <label>Password</label>
                     <input type="password"/>
-                    <button className="settingsSubmit">Update</button>
+                    <button className="settingsSubmitButton" type="submit">Update</button>
                 </form>
             </div>
             <Sidebar/>
