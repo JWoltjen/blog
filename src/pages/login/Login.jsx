975d680192b83e0feed14bbf4776131f369cd844
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-30 16:20:12 -0600

    add basic jsx elements for login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,14 @@ import './Login.css'
 export default function Login() {
     return (
         <div className="login">
-            login
+            <form className="loginForm">
+                <label> Email</label>
+                <input type="text" placeholder=" enter your email"/>
+                <label> Password</label>
+                <input type="password" placeholder="enter your password..."/>
+                <button className="loginButton">Login</button>
+            </form>
+            <button className="loginRegisterButton">Register</button>
         </div>
     )
 }
